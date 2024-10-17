472146e79e88aa88e4c9b709591dcadbaa830c51
jpsferreira <j.ferreira@fe.up.pt>
2024-10-17 15:54:12 +0100

    jacobi method

--- TP3_sistemas/sist_eq_lineares.m
+++ TP3_sistemas/sist_eq_lineares.m
@@ -12,21 +12,23 @@ B = [6;-2;0;9];
 
 x1 = subs_ts(Am,Bm);
 
-%[ee2,ff2,mm2,dd2,ind]=gauss_pt(e,f);
+[ee2,ff2,mm2,dd2,ind]=gauss_pt(A,B);
 
-%x2 = subs_tts(ee2,ff2,ind);
+x2 = subs_tts(ee2,ff2,ind);
 
-% 
-% x0 =[1
-%     1
-%     1
-%     1];
-% tol=1e-5;
-% maxiter=100;
-% 
-% a=[10 1 0 0;1 7 -1 0;0 -1 10 1; 0 0 1 7];
-% b=[6 8.5 -10 6]';
-% 
-% [x3,erro,iter] = jacobi(a,b,x0,tol,maxiter);
-% 
-% [x4,erro1,iter1] = gauss_seidel(a,b,x0,tol,maxiter);
+
+x0 =[1
+    1
+    1
+    1];
+tol=1e-5;
+maxiter=100;
+
+a=[10 1 0 0;1 7 -1 0;0 -1 10 1; 0 0 1 7];
+b=[6 8.5 -10 6]';
+a=[-20 2 -1 1;1 8 0 1;-1 2 10 1; 0 1 1 12];
+b=[6 -2 0 9]';
+x0=[0.1 -1.2 0 0.9]';
+[x3,erro,iter] = jacobi(a,b,x0,tol,maxiter);
+
+[x4,erro1,iter1] = gauss_seidel(a,b,x0,tol,maxiter);
